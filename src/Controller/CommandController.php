eb3f7fe21a2e60fbd0761888d5fc1e432d309e47
Zerlix <t.hemm@t-online.de>
2025-01-16 15:04:31 +0100

    new method validateCommand() in CommandController

--- src/Controller/CommandController.php
+++ src/Controller/CommandController.php
@@ -9,37 +9,44 @@ abstract class CommandController
     {
         try {
 
-            // validate command 
-            foreach ($command as $cmd) {
-                if (!preg_match('/^[a-zA-Z0-9_\-\/\s:.]+$/', $cmd)) {
-                    throw new Exception('Invalid command');
-                }
-            }
+            // validate the command
+            $this->validateCommand($command);
 
+            // file descriptors
             $descriptorspec = [
                 0 => ["pipe", "r"],  // stdin
                 1 => ["pipe", "w"],  // stdout
                 2 => ["pipe", "w"]   // stderr
             ];
 
+            // execute the command
             $process = proc_open(implode(' ', $command), $descriptorspec, $pipes);    
             
+            // check if the command was executed successfully
             if (!is_resource($process)) {
                 throw new Exception("Unable to execute command: $command");
             }
 
+            // read the output
             $output = stream_get_contents($pipes[1]);
+            
+            // read the error output
             $errorOutput = stream_get_contents($pipes[2]);
             
-            fclose($pipes[1]);
-            fclose($pipes[2]);
+            // close the pipes
+            foreach ($pipes as $pipe) {
+                if (is_resource($pipe)) {
+                    fclose($pipe);
+                }
+            }
 
+            // close the process
             $returnVar = proc_close($process);
-            
             if ($returnVar !== 0) {
                 throw new Exception($errorOutput);
             }
 
+            // return the output
             return ['status' => 'success', 'output' => $output];
 
           // catch any exceptions  
@@ -48,6 +55,16 @@ abstract class CommandController
                 'status' => 'error',
                 'message' => $e->getMessage()
             ];
+        }
+    }
+
+    // validate command
+    private function validateCommand(array $command): void
+    {
+        foreach ($command as $cmd) {
+            if (!preg_match('/^[a-zA-Z0-9_\-\/\s:.]+$/', $cmd)) {
+                throw new Exception('Invalid command');
+            }
         }
     }
 }
