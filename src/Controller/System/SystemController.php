e015c3a8dc3fcca12a3c1179cd0d371fbbc0b67c
Zerlix <t.hemm@t-online.de>
2025-01-17 21:49:08 +0100

    refactor code

--- src/Controller/System/SystemController.php
+++ src/Controller/System/SystemController.php
@@ -13,85 +13,103 @@ class SystemController extends CommandController
 
         // api/system/uptime
         if ($route === 'uptime' && $method === 'GET') {
-            $output = $this->executeCommand(['uptime']);
-            $outputString = implode("\n", array_map('trim', $output));
-
-            // format the output
-            if (preg_match('/up\s+(.*?),\s+\d+\s+user.*load\s+average:\s+([\d.]+),\s+([\d.]+),\s+([\d.]+)/', $outputString, $matches)) {
-                $formattedOutput = [
-                    'uptime' => $matches[1],
-                    'load_average' => [
-                        '1min' => $matches[2],
-                        '5min' => $matches[3],
-                        '15min' => $matches[4]
-                    ]
-                ];
-                return ['status' => 'success', 'data' => $formattedOutput];
-            } else {
-                return ['status' => 'error', 'message' => 'Unable to parse uptime output'];
-            }
+            return $this->handleUptime();
         }
 
         // api/system/memory
         if ($route === 'memory' && $method === 'GET') {
-            $output = $this->executeCommand(['free', '-h', '-t', '-w']);
-            $outputString = implode("\n", array_map('trim', $output));
-
-            // format the output
-            if (preg_match('/Mem:\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/', $outputString, $matches)) {
-                $formattedOutput = [
-                    'total' => $matches[1],
-                    'used' => $matches[2],
-                    'free' => $matches[3],
-                    'shared' => $matches[4],
-                    'buff_cache' => $matches[5],
-                    'available' => $matches[6]
-                ];
-                return ['status' => 'success', 'data' => $formattedOutput];
-            } else {
-                return ['status' => 'error', 'message' => 'Unable to parse memory output'];
-            }
+            return $this->handleMemory();
         }
-
 
         // api/system/cpu
         if ($route === 'cpu' && $method === 'GET') {
-            $output = $this->executeCommand(['lscpu']);
-            $outputString = implode("\n", array_map('trim', $output));
-
-            // format the output
-            if (preg_match('/Model name:\s+(.*)/', $outputString, $matches)) {
-                $formattedOutput = [
-                    'model' => $matches[1]
-                ];
-                return ['status' => 'success', 'data' => $formattedOutput];
-            } else {
-                return ['status' => 'error', 'message' => 'Unable to parse cpu output'];
-            }
+            return $this->handleCpu();
         }
 
         // api/system/cpudetails
         if ($route === 'cpudetails' && $method === 'GET') {
-            $output = $this->executeCommand(['lscpu']);
-            $outputString = implode("\n", array_map('trim', $output));
-
-            // format the output
-            $formattedOutput = [];
-            if (preg_match_all('/(.+):\s+(.*)/', $outputString, $matches)) {
-                foreach ($matches[1] as $index => $key) {
-                    $formattedOutput[$key] = $matches[2][$index];
-                }
-                return ['status' => 'success', 'data' => $formattedOutput];
-            } else {
-                return ['status' => 'error', 'message' => 'Unable to parse cpu details output'];
-            }
+            return $this->handleCpuDetails();
         }
-        
 
         // return an error if the route is not found
         return [
             'status' => 'error',
             'message' => 'Route not found'
         ];
+    }
+
+    private function handleUptime(): array
+    {
+        $output = $this->executeCommand(['uptime']);
+        $outputString = implode("\n", array_map('trim', $output));
+
+        // format the output
+        if (preg_match('/up\s+(.*?),\s+\d+\s+user.*load\s+average:\s+([\d.]+),\s+([\d.]+),\s+([\d.]+)/', $outputString, $matches)) {
+            $formattedOutput = [
+                'uptime' => $matches[1],
+                'load_average' => [
+                    '1min' => $matches[2],
+                    '5min' => $matches[3],
+                    '15min' => $matches[4]
+                ]
+            ];
+            return ['status' => 'success', 'data' => $formattedOutput];
+        } else {
+            return ['status' => 'error', 'message' => 'Unable to parse uptime output'];
+        }
+    }
+
+    private function handleMemory(): array
+    {
+        $output = $this->executeCommand(['free', '-h', '-t', '-w']);
+        $outputString = implode("\n", array_map('trim', $output));
+
+        // format the output
+        if (preg_match('/Mem:\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/', $outputString, $matches)) {
+            $formattedOutput = [
+                'total' => $matches[1],
+                'used' => $matches[2],
+                'free' => $matches[3],
+                'shared' => $matches[4],
+                'buff_cache' => $matches[5],
+                'available' => $matches[6]
+            ];
+            return ['status' => 'success', 'data' => $formattedOutput];
+        } else {
+            return ['status' => 'error', 'message' => 'Unable to parse memory output'];
+        }
+    }
+
+    private function handleCpu(): array
+    {
+        $output = $this->executeCommand(['lscpu']);
+        $outputString = implode("\n", array_map('trim', $output));
+
+        // format the output
+        if (preg_match('/Model name:\s+(.*)/', $outputString, $matches)) {
+            $formattedOutput = [
+                'model' => $matches[1]
+            ];
+            return ['status' => 'success', 'data' => $formattedOutput];
+        } else {
+            return ['status' => 'error', 'message' => 'Unable to parse cpu output'];
+        }
+    }
+
+    private function handleCpuDetails(): array
+    {
+        $output = $this->executeCommand(['lscpu']);
+        $outputString = implode("\n", array_map('trim', $output));
+
+        // format the output
+        $formattedOutput = [];
+        if (preg_match_all('/(.+):\s+(.*)/', $outputString, $matches)) {
+            foreach ($matches[1] as $index => $key) {
+                $formattedOutput[$key] = $matches[2][$index];
+            }
+            return ['status' => 'success', 'data' => $formattedOutput];
+        } else {
+            return ['status' => 'error', 'message' => 'Unable to parse cpu details output'];
+        }
     }
 }
--- src/Controller/Virsh/VirshController.php
+++ src/Controller/Virsh/VirshController.php
@@ -23,59 +23,40 @@ class VirshController extends CommandController
         }
 
         // api/virsh/start/{name_or_uuid}
-        if (preg_match('/^start\/(.+)$/', $route, $matches)) {
-            $domainIdentifier = $matches[1];
-            $domains = $this->executeCommand(['virsh', '-c', $this->uri, 'list', '--all', '--name', '--uuid']);
-            var_dump($domains);
-            if ($domains['status'] === 'success') {
-                $lines = explode("\n", $domains['output']);
-                foreach ($lines as $line) {
-                    $parts = preg_split('/\s+/', trim($line));
-                    if (count($parts) == 2) {
-                        $uuid = $parts[0];
-                        $name = $parts[1];
-                        if (strcasecmp($name, $domainIdentifier) == 0 || $uuid === $domainIdentifier) {
-                            return $this->executeCommand(['virsh', '-c', $this->uri, 'start', $name]);
-                        }
-                    }
-                }
-                return ['status' => 'error', 'message' => "Domain '$domainIdentifier' not found"];
-            } else {
-                return ['status' => 'error', 'message' => 'Unable to list domains'];
-            }
+        if (preg_match('/^(start|shutdown)\/(.+)$/', $route, $matches)) {
+            $action = $matches[1];
+            $domainIdentifier = $matches[2];
+            return $this->handleDomainAction($action, $domainIdentifier);
         }
 
-
-        // api/virsh/shutdown/{name_or_uuid}
-        if (preg_match('/^shutdown\/(.+)$/', $route, $matches)) {
-            $domainIdentifier = $matches[1];
-            $domains = $this->executeCommand(['virsh', '-c', $this->uri, 'list', '--all', '--name', '--uuid']);
-
-            if ($domains['status'] === 'success') {
-                $lines = explode("\n", $domains['output']);
-                foreach ($lines as $line) {
-                    $parts = preg_split('/\s+/', trim($line));
-                    if (count($parts) == 2) {
-                        $uuid = $parts[0];
-                        $name = $parts[1];
-                        if (strcasecmp($name, $domainIdentifier) == 0 || $uuid === $domainIdentifier) {
-                            return $this->executeCommand(['virsh', '-c', $this->uri, 'shutdown', $name]);
-                        }
-                    }
-                }
-                return ['status' => 'error', 'message' => "Domain '$domainIdentifier' not found"];
-            } else {
-                return ['status' => 'error', 'message' => 'Unable to list domains'];
-            }
-        }
-
-
-        
         // return an error if the route is not found
         return [
             'status' => 'error',
             'message' => 'Route not found'
         ];
+    }
+
+
+    // parse the output of the virsh domstats action start/stop
+    private function handleDomainAction(string $action, string $domainIdentifier): array
+    {
+        $domains = $this->executeCommand(['virsh', '-c', $this->uri, 'list', '--all', '--name', '--uuid']);
+        if ($domains['status'] === 'success') {
+            $lines = explode("\n", $domains['output']);
+            foreach ($lines as $line) {
+                $parts = preg_split('/\s+/', trim($line));
+                if (count($parts) == 2) {
+                    $uuid = $parts[0];
+                    $name = $parts[1];
+                    if (strcasecmp($name, $domainIdentifier) == 0 || $uuid === $domainIdentifier) {
+                        return $this->executeCommand(['virsh', '-c', $this->uri, $action, $name]);
+                    }
+                }
+            }
+            return ['status' => 'error', 'message' => "Domain '$domainIdentifier' not found"];
+        } else {
+            return ['status' => 'error', 'message' => 'Unable to list domains'];
+        }
     }
 
     // parse the output of the virsh domstats command
