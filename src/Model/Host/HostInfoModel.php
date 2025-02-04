9007b0e165a39ff91065cd1cbab013bfccc394a6
Zerlix <t.hemm@t-online.de>
2025-02-04 14:39:45 +0100

    fix: Add return type hint for handle method in HostInfoModel - Added array<string, mixed> return type hint for handle method in HostMemModel - add declare(strict_types=1)

--- src/Model/Host/HostInfoModel.php
+++ src/Model/Host/HostInfoModel.php
@@ -1,10 +1,19 @@
 <?php
+declare(strict_types=1);
+
 namespace Zerlix\KvmDash\Api\Model\Host;
 
 use Zerlix\KvmDash\Api\Model\CommandModel;
 
 class HostInfoModel extends CommandModel
 {
+    /**
+     * Handle the host information request
+     * 
+     * @param string $route
+     * @param string $method
+     * @return array<string, mixed>
+     */
     public function handle(string $route, string $method): array
     {
         $output = $this->executeCommand(['hostnamectl status --json=pretty']);
